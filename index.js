0d210ce19caca3827ed90c6c4b62ea38091eaedc
Miguel Angel <108877252+elalgrere@users.noreply.github.com>
2022-09-21 13:06:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,123 +1,123 @@
-const express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	uuid = require('uuid'),
-	morgan = require('morgan');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
+const app = express();
 app.use(bodyParser.json());
-app.use(morgan('common'));
 
-// define arrays of user and movie objects that later get handed over in JSON
+
 let users = [
-	{
-		id: 1,
-		name: 'Kim',
-		favoriteMovies: [],
-	},
-	{
-		id: 2,
-		name: 'Joe',
-		favoriteMovies: 'The Fountain',
-	},
-];
+    {
+      name: 'Isabel',
+      favmovie: ['The girl with the Pearl Earring']
+    },
+    {
+      name: 'Jonathan',
+      favmovie: ['Goyas Ghost']
+    },
+    {
+      name: 'Paul',
+      famovie: ['Pulp Fiction']
+    }
+  ];
+  
+  let movies = [
+    {
+      "Title": "The Girl with a Pearl Earring",
+      "Description":"A young peasant maid working in the house of painter Johannes Vermeer becomes his talented assistant and the model for one of his most famous works.",
+  
+      "Genre": {
+      "Name":"Drama",
+      "Description": "In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone. Drama of this kind is usually qualified with additional terms that specify its particular super-genre, macro-genre, or micro-genre.  To these ends, a primary element in a drama is the occurrence of conflict—emotional, social, or otherwise—and its resolution in the course of the storyline.",
+    },
+  
+      "Director": {
+        "Name":"Peter Webber",
+        "Bio":"Webber estrenó su primer cortometraje, The Zebra Man, sobre la vida del artista friki Horace Ridler, protagonizad por Minnie Driver. En 2012 regresó a la gran pantalla con el drama bélico Emperor, con Tommy Lee Jones como protagonista. En 2016 dirigió la miniserie Tutankhamun, protagonizada por Sam Neill. En 2018 se trasladó a Bogotá, Colombia, para rodar la película Pickpockets. El director ya había visitado Colombia en 2009 para filmar el documental The Sand and the Rain sobre la tribu amazónica Macuna. En 2019 inició el rodaje de una película de suspenso titulada Fremason para Lionsgate.",
+        "Birth": 1968
+    },
+        "imageURl":"https://www.google.com/search?q=Peter+Webber&sxsrf=ALiCzsbH3y2fQ0qkbccGy-0rm4QUs3SwPg:1662127610542&source=lnms&tbm=isch&sa=X&ved=2ahUKEwifubDuo_b5AhV1XfEDHd9zBbEQ_AUoAXoECAIQAw&biw=1280&bih=623&dpr=2#imgrc=IXH_9oF5l5DJbM",
+        "Featured": false
+    },
+  
+    {
+        "Title": "Pulp fiction",
+        "Description": "Pulp fiction','American film written and directed by Quentin Tarantino . The script is based on stories that Tarantino himself developed in collaboration with Roger Avary during the years 1992 and 1993, including scenes that had originally been written for True Romance . Based on a non - linear narrative , the film intertwines several stories whose protagonists are members of organized crime in Los Angeles . The stylized dialogues, the mixture of humor and violence and its tone ironic set the film apart. Its name derives from the popular mid- 20th century pulp literature magazines and hard boiled graphic novels known for their graphic violence and harsh prose",
+        "Genre": {
+        "Name": "Action",
+        "Description": "Action films are a film genre where action sequences, such as fighting, stunts, car chases or explosions, take precedence over elements like characterisation or complex plotting. The action typically involves individual efforts on the part of the hero, in contrast with most war films. The genre is closely linked with the thriller and adventure film genres."
+     },
+        "Director": {
+         "Name": "Quetin Tarantino",
+         "Bio": "In 1992 he began his career as an independent filmmaker with the premiere of Reservoir Dogs, considered by Empire magazine as 'the best independent film of all time'. His popularity only grew with his second feature film, Pulp Fiction ( 1994), a black comedy anthology that became a huge success with critics and audiences, as well as a fundamental piece of popular culture. Entertainment Weekly designated it as the best film released between 1983 and 2008, and some critics of the British Film Institute placed it at number 127 of the best films of all time.23 In Jackie Brown (1997), Tarantino paid homage to the blaxploitation genre",
+         "Birth": "27-03-1963",
+         "Death": ""
+     },
+     "ImagePath": "https://es.wikipedia.org/wiki/Pulp_Fiction#/media/Archivo:Pulp_Fiction_Logo.svg",
+     "Feature": true
+    },
+    
+    {
+      "Title":"Goyas Ghost",
+      "Description":"Goya's Ghosts is a 2006 biographical drama film, directed by Miloš Forman (his final directorial feature before his death in 2018), and written by him and Jean-Claude Carrière. The film stars Javier Bardem, Natalie Portman and Stellan Skarsgård, and was filmed on location in Spain during late 2005. The film was written, produced, and performed in English although it is a Spanish production. Although the historical setting of the film is authentic, the story about Goya trying to defend a model is fictional, as are the characters Brother Lorenzo and the Bilbatúa family.",
+  
+      "Genre": {
+        "Name":"Biography",
+        "Description": "A biographical film or biopic, is a film that dramatizes the life of a non-fictional or historically-based person or people. Such films show the life of a historical person and the central character's real name is used.[2] They differ from docudrama films and historical drama films in that they attempt to comprehensively tell a single person's life story or at least the most historically important years of their lives",
+      },
+      "Director": {
+        "Name":"Miloš Forman",
+        "Bio":"Jan Tomáš Miloš Forman was a Czech and American film director, screenwriter, actor, and professor who rose to fame in his native Czechoslovakia before emigrating to the United States in 1968. Forman was an important figure in the Czechoslovak New Wave. Film scholars and Czechoslovak authorities saw his 1967 film The Firemen's Ball as a biting satire on Eastern European Communism. The film was initially shown in theatres in his home country in the more reformist atmosphere of the Prague Spring. However, it was later banned by the Communist government after the invasion by the Warsaw Pact countries in 1968.[3] Forman was subsequently forced to leave Czechoslovakia for the United States, where he continued making films, gaining wider critical and financial success. In 1975, he directed One Flew Over the Cuckoo's Nest (1975) starring Jack Nicholson as a patient in a mental institution. The film received widespread acclaim, and was the second in history to win all five major Academy Awards: Best Picture, Director, Screenplay, Actor in Leading Role, and Actress in Leading Role. In 1978, he directed the anti-war musical Hair which premiered at the 1979 Cannes Film Festival. In 1981, he directed the turn of the century drama film, Ragtime, which was known for its large ensemble cast. The film went on to receive eight Academy Award nominations. His next feature was a period biographical film, Amadeus (1984), based on the life of famed classical musician Wolfgang Amadeus Mozart starring Tom Hulce, and F. Murray Abraham. The film was both a critical and financial success earning eleven nominations with eight wins including for Best Picture, and another win for Forman as Best Director. In 1996, Forman received another Academy Award nomination for Best Director for The People vs. Larry Flynt (1996). Throughout Forman's career he won two Academy Awards, three Golden Globe Awards, Grand Prix at the Cannes Film Festival, Golden Bear at the Berlin Film Festival, a British Academy Film Award, a César Award, David di Donatello Award, and the Czech Lion.[4]",
+        "Birth": 1968
+      },
+        "imageURl":"https://www.google.com/search?q=Peter+Webber&sxsrf=ALiCzsbH3y2fQ0qkbccGy-0rm4QUs3SwPg:1662127610542&source=lnms&tbm=isch&sa=X&ved=2ahUKEwifubDuo_b5AhV1XfEDHd9zBbEQ_AUoAXoECAIQAw&biw=1280&bih=623&dpr=2#imgrc=IXH_9oF5l5DJbM",
+        "Featured": false
+      },
+  ];
 
-let movies = [
-	{
-		Title: 'The Fountain',
-		Description: 'Description 1',
-		Genre: {
-			Name: 'Drama',
-			Description: 'Description Genre Drama',
-		},
-		Director: {
-			Name: 'Director 1',
-			Bio: 'Bio Director 1',
-			Birth: 1969,
-		},
-	},
-	{
-		Title: 'Title 2',
-		Description: 'Description 2',
-		Genre: {
-			Name: 'Action',
-			Description: 'Description Genre Action',
-		},
-		Director: {
-			Name: 'Director 2',
-			Bio: 'Bio Director 2',
-			Birth: 1972,
-		},
-	},
-	{
-		Title: 'Title 3',
-		Description: 'Description 3',
-		Genre: {
-			Name: 'Drama',
-			Description: 'Description Genre Drama',
-		},
-		Director: {
-			Name: 'Director 3',
-			Bio: 'Bio Director 3',
-			Birth: 1980,
-		},
-	},
-	{
-		Title: 'Title 4',
-		Description: 'Description 4',
-		Genre: {
-			Name: 'Drama',
-			Description: 'Description Genre Drama',
-		},
-		Director: {
-			Name: 'Director 4',
-			Bio: 'Bio Director 4',
-			Birth: 1981,
-		},
-	},
-	{
-		Title: 'Title 5',
-		Description: 'Description 5',
-		Genre: {
-			Name: 'Drama',
-			Description: 'Description Genre Drama',
-		},
-		Director: {
-			Name: 'Director 5',
-			Bio: 'Bio Director 5',
-			Birth: 1982,
-		},
-	},
-];
+ //READ
 
-// CREATE new user
-app.post('/users', (req, res) => {
-	const newUser = req.body;
-
-	if (newUser.name) {
-		newUser.id = uuid.v4();
-		users.push(newUser);
-		res.status(201).json(newUser);
-	} else {
-		res.status(400).send('New user requires name');
-	}
+// Get all movies
+app.get('/movies', (req, res) => {
+   res.status(200).json(movies);
+});
+//READ
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = movies.find( movie.Title == title );
+     
+    if(movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('no such movie'); 
+    }
+    
 });
 
-// UPDATE user
-app.put('/users/:id', (req, res) => {
-	const { id } = req.params;
-	const updatedUser = req.body;
-
-	let user = users.find((user) => user.id == id);
-
-	if (user) {
-		user.name = updatedUser.name;
-		res.status(200).json(user);
-	} else {
-		res.status(400).send('User not found');
-	}
+app.get('/movies/genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find( movie => movie.Genre.Name == genreName );
+      
+    if(genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('no such genre'); 
+    }
+    
 });
 
-// CREATE favorite movie
+app.get('/movies/directors/:directorName', (req, res) => {
+	const { directorName } = req.params;
+	const director = movies.find(
+		(movie) => movie.Director.Name === directorName
+	).Director;
+
+	if (director) {
+		res.status(200).json(director);
+	} else {
+		res.status(404).send('Director not in database');
+	}
+});
+//CREATE favorite movie
 app.post('/users/:id/:movieTitle', (req, res) => {
 	const { id, movieTitle } = req.params;
 
@@ -133,25 +133,33 @@ app.post('/users/:id/:movieTitle', (req, res) => {
 	}
 });
 
-// DELETE favorite movie
-app.delete('/users/:id/:movieTitle', (req, res) => {
-	const { id, movieTitle } = req.params;
+//CREATE new user
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+  
+  if(!newUser.name) {
+       newUser.id = uuid.v4();
+       users.push(newUser);
+       res.status(201).json(newUser);
+  } else {
+       res.status(400).send('New user name')
+  }
+});
+//UPDATE user
+app.put('/users/:id', (req, res) => {
+	const { id } = req.params;
+	const updatedUser = req.body;
 
 	let user = users.find((user) => user.id == id);
 
 	if (user) {
-		user.favoriteMovies = user.favoriteMovies.filter(
-			(title) => title !== movieTitle
-		);
-		res.status(200).send(
-			`${movieTitle} has been removed from user ${id}'s array`
-		);
+		user.name = updatedUser.name;
+		res.status(200).json(user);
 	} else {
 		res.status(400).send('User not found');
 	}
 });
-
-// DELETE user
+//DELETE user
 app.delete('/users/:id', (req, res) => {
 	const { id } = req.params;
 
@@ -165,56 +173,26 @@ app.delete('/users/:id', (req, res) => {
 	}
 });
 
-// READ all movies
-app.get('/movies', (req, res) => {
-	res.status(200).json(movies);
-});
+//DELETE favorite movie
+app.post('/users/:id/:movieTitle', (req, res) => {
+	const { id, movieTitle } = req.params;
 
-// READ one movie by title
-app.get('/movies/:title', (req, res) => {
-	const { title } = req.params;
-	const movie = movies.find((movie) => movie.Title === title);
+	let user = users.find((user) => user.id == id);
 
-	if (movie) {
-		res.status(200).json(movie);
+	if (user) {
+		user.favoriteMovies.push(movieTitle);
+		res.status(200).send(
+			`${movieTitle} has been added to user ${id}'s array`
+		);
 	} else {
-		res.status(404).send('Movie not in database');
+		res.status(400).send('User not found');
 	}
 });
 
-// READ genre by name
-app.get('/movies/genre/:genreName', (req, res) => {
-	const { genreName } = req.params;
-	const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
 
-	if (genre) {
-		res.status(200).json(genre);
-	} else {
-		res.status(404).send('Genre not in database');
-	}
-});
 
-// READ director by name
-app.get('/movies/directors/:directorName', (req, res) => {
-	const { directorName } = req.params;
-	const director = movies.find(
-		(movie) => movie.Director.Name === directorName
-	).Director;
 
-	if (director) {
-		res.status(200).json(director);
-	} else {
-		res.status(404).send('Director not in database');
-	}
-});
+app.listen(8080,() => console.log('listen on 8080'))
 
-// error handler
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('An error was encountered!');
-});
 
-// listen for requests
-app.listen(8080, () => {
-	console.log('Your app is listening on port 8080.');
-});
+
